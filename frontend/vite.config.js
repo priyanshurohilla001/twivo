8a2298a825c61b1b003f9431a41c94bb92966616
priyanshu <priyanshurohilla001@gmail.com>
2025-04-03 20:20:34 +0530

    https in vite for auth0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "node:path"
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    host: 'myapp.example', // Custom domain
-    port: 5173,            // Port number
+    https:true
   },
 })
